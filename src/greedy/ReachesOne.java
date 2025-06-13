f9e9e4be2e16203575de68849a906fe68a1fb908
moonju lee <mjlee4327@gmail.com>
2025-06-13 23:54:56 +0900

    refactor reach one

--- src/greedy/ReachesOne.java
+++ src/greedy/ReachesOne.java
@@ -7,23 +7,20 @@ public class ReachesOne {
 
     private static int solution(int[] arr) {
         int result = 0;
+        int target = 0;
+        while (true) {
+            target = (arr[0] / arr[1]) * arr[1];
+            result += arr[0] - target;
 
-        // 무조건 arr[0]을 arr[1]로 나누는 것이 arr[0] 숫자를 줄이는 최선의 방식이다.
-        while (arr[0] >= arr[1]) {
-            // arr[0]이 arr[1]로 나눠 떨어지지 않으면 1씩 감소시켜 나누어 떨어지는 수로 만든다.
-            while (arr[0] % arr[1] != 0) {
-                arr[0]--;
-                result++;
+            arr[0] = target;
+            if(arr[0] < arr[1]) {
+                break;
             }
+
             arr[0] /= arr[1];
             result++;
         }
-
-        // 마지막에 arr[1]보다 작고 1보다 클 경우 1씩 감소 시켜준다.
-        while (arr[0] > 1) {
-            arr[0]--;
-            result++;
-        }
+        result += arr[0] - 1;
 
         return result;
     }
