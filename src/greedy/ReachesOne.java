98b7b0bd304d622ca0ab9bca11e0515c94d592d0
moonju lee <mjlee4327@gmail.com>
2025-06-14 23:10:31 +0900

    repeat reach one

--- src/greedy/ReachesOne.java
+++ src/greedy/ReachesOne.java
@@ -7,18 +7,19 @@ public class ReachesOne {
 
     private static int solution(int[] arr) {
         int result = 0;
-        int target = 0;
+        int target;
+
         while (true) {
             target = (arr[0] / arr[1]) * arr[1];
             result += arr[0] - target;
-
             arr[0] = target;
+
             if(arr[0] < arr[1]) {
                 break;
             }
 
             arr[0] /= arr[1];
-            result++;
+            result += 1;
         }
         result += arr[0] - 1;
 
