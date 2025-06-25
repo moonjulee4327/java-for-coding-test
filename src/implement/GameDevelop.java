40e40117df56dc7f53e4c9153519d5cc4cc81121
moonju lee <mjlee4327@gmail.com>
2025-06-26 03:18:51 +0900

    my game develop

--- src/implement/GameDevelop.java
+++ src/implement/GameDevelop.java
@@ -3,73 +3,78 @@ package implement;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class GameDevelop {
-    // 북, 동, 남, 서
-    private static final int[] dx = {-1, 0, 1, 0};
-    private static final int[] dy = {0, 1, 0, -1};
+    private static int[] dx = {-1, 0, 1, 0};
+    private static int[] dy = {0, 1, 0, -1};
+    private static int n, m;
+    private static int direction;
     private static int[][] map = new int[50][50];
     private static int[][] visited = new int[50][50];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String nm = br.readLine();
-        String[] nmArr = nm.split("");
-        int n = Integer.parseInt(nmArr[0]);
-        String[] position = br.readLine().split(" ");
-        int x = Integer.parseInt(position[0]);
-        int y = Integer.parseInt(position[1]);
-        int direction = Integer.parseInt(position[2]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        visited[x][y] = 1;
+        direction = Integer.parseInt(st.nextToken());
 
         for(int i = 0; i < n; i++) {
-            String[] row = br.readLine().split(" ");
-            for(int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(row[j]);
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        br.close();
 
-        System.out.println(move(x, y, direction, map));
+        System.out.println(move(x, y, map));
     }
 
-    private static int move(int x, int y, int direction, int[][] map) {
-        int count = 1;
-        int terns = 0;
-        while (true) {
-            // 왼쪽 방향으로 회전
-            direction--;
-            if(direction == -1) {
-                direction = 3;
-            }
+    private static void turnLeft() {
+        direction--;
+        if(direction == -1) {
+            direction = 3;
+        }
+    }
 
-            // 이동 할 칸 확인 후 이동
+    private static int move(int x, int y, int[][] map) {
+        int cnt = 1;
+        int turns = 0;
+        while (true) {
+            turnLeft();
             int nx = x + dx[direction];
             int ny = y + dy[direction];
 
-            if(map[nx][ny] == 0 && visited[nx][ny] == 0) {
+            if(map[nx][ny] == 0 && visited[nx][ny] == 0 && nx >= 0 && nx < n && ny >= 0 && ny < m) {
                 x = nx;
                 y = ny;
-                visited[x][y] = 1;
-                count++;
+                visited[nx][ny] = 1;
+                cnt++;
+                turns = 0;
                 continue;
-            }else { // 바다이거나 가본 경우
-                terns++;
+            }else {
+                turns++;
             }
 
-            // 네 방향을 갈 수 없는 경우
-            if(terns == 4) {
+            if(turns == 4 && nx >= 0 && nx < n && ny >= 0 && ny < m) {
                 nx = x - dx[direction];
                 ny = y - dy[direction];
-                // 뒤로 이동
                 if(map[nx][ny] == 0) {
                     x = nx;
                     y = ny;
                 }else {
                     break;
                 }
-                terns = 0;
+                turns = 0;
             }
         }
 
-        return count;
+        return cnt;
     }
 }
