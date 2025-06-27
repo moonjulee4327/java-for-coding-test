ecc0b17e9bcd194a72bbc6d71bf5a66c07a7a106
moonju lee <mjlee4327@gmail.com>
2025-06-28 02:34:52 +0900

    exam dfs

--- src/dfsbfs/ExamDfs.java
+++ src/dfsbfs/ExamDfs.java
@@ -1,31 +1,51 @@
 package dfsbfs;
 
-public class ExamDfs {
-    public static void main(String[] args) {
-        int[][] graph = {
-                {},
-                {2, 3, 8},
-                {1, 7},
-                {1, 4, 5},
-                {3, 5},
-                {3, 4},
-                {7},
-                {2, 6, 8},
-                {1, 7}
-        };
+import java.util.ArrayList;
 
-        boolean[] visited = new boolean[graph.length];
-        dfs(graph, 1, visited);
+public class ExamDfs {
+    private static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+    private static boolean[] visited = new boolean[9];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+
+        graph.get(1).add(2);
+        graph.get(1).add(3);
+        graph.get(1).add(8);
+
+        graph.get(2).add(1);
+        graph.get(2).add(7);
+
+        graph.get(3).add(1);
+        graph.get(3).add(4);
+        graph.get(3).add(5);
+
+        graph.get(4).add(3);
+        graph.get(4).add(5);
+
+        graph.get(5).add(3);
+        graph.get(5).add(4);
+
+        graph.get(6).add(7);
+
+        graph.get(7).add(2);
+        graph.get(7).add(6);
+        graph.get(7).add(8);
+
+        graph.get(8).add(1);
+        graph.get(8).add(7);
+
+        dfs(1);
     }
 
-    private static void dfs(int[][] graph, int num, boolean[] visited) {
-        visited[num] = true;
-        System.out.print(num + " ");
-        for(int i = 0; i < graph[num].length; i++) {
-            int next = graph[num][i];
-            if(!visited[next]) {
-                dfs(graph, next, visited);
-            }
+    private static void dfs(int x) {
+        visited[x] = true;
+        System.out.print(x + " ");
+        for(int i = 0; i < graph.get(x).size(); i++) {
+            int y = graph.get(x).get(i);
+            if(!visited[y]) dfs(y);
         }
     }
 }
